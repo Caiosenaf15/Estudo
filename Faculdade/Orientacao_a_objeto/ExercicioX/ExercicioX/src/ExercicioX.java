4bc26aa89c9d219e0663ebe8cbfb18069177af17
Caio <caiofreitas15@yahoo.com.br>
2024-10-05 22:21:16 -0300

    mudanca de pasta

--- Faculdade/Orientacao_a_objeto/ExercicioX/ExercicioX/src/ExercicioX.java
+++ Faculdade/Orientacao_a_objeto/ExercicioX/ExercicioX/src/ExercicioX.java
@@ -1,20 +1,20 @@
 public class ExercicioX {
     public static void main(String[] args) throws Exception {
         for(int i = 100; i < 201; i++ ){
-            //System.out.println(i);
+            System.out.println(i); //Printa os numeros de 100 a 200
         }
         int soma = 0;
         for(int i = 0; i < 501; i++){
-            soma = soma + i;
+            soma = soma + i; 
         }
-        System.out.println(soma);
+        System.out.println(soma); //Printa a soma dos numeros de 1 a 500
         int qtd = 0;
         for(int i = 1; i < 3001; i++){
             if(i % 5 == 0)
                 qtd++;
-                System.out.println(i);
+                System.out.println(i); //Printa os numeros divisiveis por 5
         }
-        System.out.println(qtd);
+        System.out.println(qtd); //Printa a quantidade dos divisiveis por 5
     }
         
 } 
